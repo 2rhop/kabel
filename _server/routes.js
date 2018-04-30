2df23e25a4134a5e58994ffd0bd4fd93b8044cd7
Rene Ricardo <i2rhop@gmail.com>
2018-04-30 11:19:24 -0400

    feat: Updating 'queries examples' in routes.js file. Setting error treatment with catch() method

--- _server/routes.js
+++ _server/routes.js
@@ -1,29 +1,51 @@
 import express from 'express'
 import knex from '../_database/db'
+import colors from '../utils/console_color'
 
 const routes = express.Router()
 
 // ROUTES
 routes
     .get('/users', (req, res) => {
-        knex.raw('select * from users').then((users) => {
-            res.json(users[0])
-        })
-    })
-    .get('/user/:id', (req, res) => {
-        knex.raw('select * from users where uid=?', [req.params.id]).then((user) => {
-            res.json(user[0])
+        knex('users').then((users) => {
+            res.json(users)
         })
     })
     .post('/user', (req, res) => {
-        knex.raw('insert into users(username,password) values(?,?)', [req.body.name, req.body.pass]).then((user) => {
-            res.json(user[0])
-        })
+        knex.insert([{ username: req.body.name, password: req.body.pass }]).into('users')
+            .then(function (user) {
+                knex('users').where('id', user[0]).then(function (user) {
+                    res.send(user)
+                })
+            }).catch(function (error) {
+                console.error(colors[0], error)
+                return res.sendStatus(400)
+            })
     })
     .delete('/user/:id', (req, res) => {
-        knex.raw('delete from users where uid=?', [req.params.id]).then((user) => {
-              res.json(user[0])
-          })
+        knex('users').where('id', [req.params.id]).del().then((user) => {
+            res.sendStatus(200)
+        }).catch(function (error) {
+            console.error(colors[0], error)
+            res.sendStatus(400)
+        })
+    })
+    .get('/user/:id', (req, res) => {
+        knex('users').where('id', req.params.id).then((user) => {
+            res.json(user)
+        })
+    })
+    .put('/user/:id', (req, res) => {
+        knex('users')
+            .where('id', req.params.id)
+            .update({
+                username: req.body.name,
+            }).then(function (user) {
+                res.sendStatus(200)
+            }).catch(function (error) {
+                console.error(colors[0], error)
+                res.sendStatus(400)
+            })
     })
 
 
