6d6afc8572737a83a965a31f9f37d93b1fb5ae7c
Rene Ricardo <i2rhop@gmail.com>
2018-04-30 12:41:18 -0400

    fix: Changing the global call of babel-node to the local call inside the project dir

--- utils/copy.js
+++ utils/copy.js
@@ -4,11 +4,13 @@ import colors from './console_color'
 //copy a file from .. to ..
 export default function copy(from, to) {
     //if the destination file is not exists
-    // if (!fs.existsSync(to))
+    if (!fs.existsSync(to))
         fs.copyFile(from, to, (err) => {
             if (err)
                 console.log(colors[0], err);
             else
                 console.log(colors[4], `copy file from ${from} to ${to}`);
         });
+    else
+        console.log(colors[2], 'The file is already exist');
 }
