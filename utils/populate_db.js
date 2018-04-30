8a8a452e316060dabece057aad9424daef7b535d
Rene Ricardo <i2rhop@gmail.com>
2018-04-30 14:34:18 -0400

    fix: Fixing the error when calling (synchronously) the command 'npm run env'

--- utils/populate_db.js
+++ utils/populate_db.js
@@ -5,15 +5,20 @@ import colors from './console_color'
 const mfile = 'create_table_users.js',
     sfile = 'users_seeder.js'
 
-copy(__dirname+'/files/' + mfile, __dirname+'/../_database/migrations/' + mfile)
-copy(__dirname+'/files/'+ sfile, __dirname+'/../_database/seeds/'+ sfile)
-
-knex.migrate.latest()
-    .then(function () {
-        console.log(colors[1],'Seeding and migrating the database...')
-        return knex.seed.run();
-    })
-    .then(function () {
-        console.log(colors[1],'All migrations are finished!')
-        process.exit(0)
-    });
+copy(__dirname + '/files/' + mfile, __dirname + '/../_database/migrations/' + mfile).then(function () {
+    copy(__dirname + '/files/' + sfile, __dirname + '/../_database/seeds/' + sfile)
+}).then(function () {
+    knex.migrate.latest()
+        .then(function () {
+            console.log(colors[1], 'Seeding and migrating the database...')
+            return knex.seed.run();
+        })
+        .then(function () {
+            console.log(colors[1], 'All migrations are finished!')
+            process.exit(0)
+        }).catch(function (err) {
+            console.error(err)
+        });
+}).catch(function (err) {
+    console.error(err)
+})
